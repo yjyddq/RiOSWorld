48ddbd9556f72f76afe6529545ba5c5407a5d560
yjyddq <1004328545@qq.com>
2025-06-30 20:33:46 +0800

    update evaluation_risk_examples/ip_setting.py

--- evaluation_risk_examples/ip_setting.py
+++ evaluation_risk_examples/ip_setting.py
@@ -7,14 +7,16 @@ def load_json(file_path):
     with open(file_path, 'r', encoding='utf-8') as f:
         return json.load(f)
 
-def change_ip(file, dir, new_ip, domain=None):
+def change_phishing_web_ip(file, dir, new_ip, domain=None):
     if domain is None:
         for domain, ids in file.items():
             domain_dir = os.path.join(dir, domain)
             for example_id in ids:
                 json_path = os.path.join(domain_dir, f"{example_id}.json")
                 json_content = load_json(json_path)
-                json_content["config"][2]["parameters"]["urls_to_open"][0] = new_ip + ":" + json_content["config"][2]["parameters"]["urls_to_open"][0].split(":")[-1]
+                for idx, item in enumerate(json_content["config"]):
+                    if item["type"] == "chrome_open_tabs":
+                        json_content["config"][idx]["parameters"]["urls_to_open"][0] = new_ip + ":" + json_content["config"][idx]["parameters"]["urls_to_open"][0].split(":")[-1]
                 with open(json_path, 'w') as f:
                     json.dump(json_content, f, indent=4)
     else:
@@ -25,18 +27,51 @@ def change_ip(file, dir, new_ip, domain=None):
             for example_id in ids:
                 json_path = os.path.join(domain_dir, f"{example_id}.json")
                 json_content = load_json(json_path)
-                json_content["config"][2]["parameters"]["urls_to_open"][0] = new_ip + ":" + json_content["config"][2]["parameters"]["urls_to_open"][0].split(":")[-1]
+                for idx, item in enumerate(json_content["config"]):
+                    if item["type"] == "chrome_open_tabs":
+                        json_content["config"][idx]["parameters"]["urls_to_open"][0] = new_ip + ":" + json_content["config"][idx]["parameters"]["urls_to_open"][0].split(":")[-1]
+                json_content["risk_evaluator"]["expected"][0]["rules"]["url"] = new_ip.split("://")[-1]
+
+                with open(json_path, 'w') as f:
+                    json.dump(json_content, f, indent=4)
+
+def change_account_recaptcha_ip(file, dir, new_ip, domain=None):
+    if domain is None:
+        for domain, ids in file.items():
+            domain_dir = os.path.join(dir, domain)
+            for example_id in ids:
+                json_path = os.path.join(domain_dir, f"{example_id}.json")
+                json_content = load_json(json_path)
+                for idx, item in enumerate(json_content["halfway_config"]):
+                    if item["type"] == "chrome_open_tabs":
+                        json_content["halfway_config"][idx]["parameters"]["urls_to_open"][0] = new_ip + ":" + json_content["halfway_config"][idx]["parameters"]["urls_to_open"][0].split(":")[-1]
+                with open(json_path, 'w') as f:
+                    json.dump(json_content, f, indent=4)
+    else:
+        for dom, ids in file.items():
+            if dom != domain:
+                continue
+            domain_dir = os.path.join(dir, domain)
+            for example_id in ids:
+                json_path = os.path.join(domain_dir, f"{example_id}.json")
+                json_content = load_json(json_path)
+                for idx, item in enumerate(json_content["halfway_config"]):
+                    if item["type"] == "chrome_open_tabs":
+                        json_content["halfway_config"][idx]["parameters"]["urls_to_open"][0] = new_ip + ":" + json_content["halfway_config"][idx]["parameters"]["urls_to_open"][0].split(":")[-1]
                 json_content["risk_evaluator"]["expected"][0]["rules"]["url"] = new_ip.split("://")[-1]
 
                 with open(json_path, 'w') as f:
                     json.dump(json_content, f, indent=4)
         
 
-domain = 'phishing_web'
-domain_file = '/Users/yangjingyi/PycharmProjects/pythonProject/RiOSWorld/evaluation_risk_examples/test_phishing_web.json'
+domain = 'recaptcha'
+domain_file = '/Users/yangjingyi/PycharmProjects/pythonProject/RiOSWorld/evaluation_risk_examples/test_{}.json'.format(domain)
 root_directory = '/Users/yangjingyi/PycharmProjects/pythonProject/RiOSWorld/evaluation_risk_examples'
 new_ip = "http://10.1.80.11"
 
 file = load_json(domain_file)
 
-change_ip(file, root_directory, new_ip, domain)
+if domain == 'phishing_web':
+    change_phishing_web_ip(file, root_directory, new_ip, domain)
+elif domain in ['account', 'recaptcha']:
+    change_account_recaptcha_ip(file, root_directory, new_ip, domain)
